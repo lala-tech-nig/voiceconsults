ee7fced68dae4dda2d5fa6f03fea6b58a2c391df
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2026-01-11 00:52:21 +0100

    fixing render never port issue

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const Summary = require('./models/Summary');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -120,5 +120,5 @@ app.post('/api/summaries', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on PORT ${PORT}`);
 });
